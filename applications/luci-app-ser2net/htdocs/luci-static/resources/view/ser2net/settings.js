1933b7d1ee7a937da522fe5f25e1e2f46b98a80b
qzyuebing <54806519+qzyuebing@users.noreply.github.com>
2025-07-18 23:22:54 +0800

    Update settings.js

--- applications/luci-app-ser2net/htdocs/luci-static/resources/view/ser2net/settings.js
+++ applications/luci-app-ser2net/htdocs/luci-static/resources/view/ser2net/settings.js
@@ -47,6 +47,7 @@ return view.extend({
 		o.value(115200);
 		o.value(230400);
 		o.value(460800);
+		o.value(921600);
 		o.default = 9600;
 
 		o = s.option(form.ListValue, "databits", _("Data bits"));
